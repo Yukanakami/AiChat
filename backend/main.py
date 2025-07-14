bf4d97c1495fc45e9d577cc8bff1ea5da080b183
Yukanakami <nyomanmaheka69@gmail.com>
2025-07-14 16:36:26 +0800

    new changes

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ import torch
 
 app = FastAPI()
 
-# Aktifkan CORS
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -15,11 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Definisi input dengan Pydantic
+
 class ChatRequest(BaseModel):
     message: str
 
-# Load model
+
 tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
 model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
 
@@ -31,14 +31,13 @@ async def chat(req: ChatRequest):
     if not user_input.strip():
         return {"response": "Tolong ketik sesuatu."}
 
-    # Tokenisasi input
     input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
 
-    # Generate output
+
     output_ids = model.generate(
     input_ids,
     max_length=100,
-    pad_token_id=tokenizer.eos_token_id  # TAMBAHKAN INI
+    pad_token_id=tokenizer.eos_token_id  
 )
 
     reply = tokenizer.decode(output_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
